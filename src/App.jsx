dcf6707bf250cd8c89f234d07bfc3e20db8c6d8b
Vinhn1 <vinhnguyen04275@gmail.com>
2025-06-24 21:36:23 +0700

    Cấu hình chung project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        {/* Header */}
+        {/* Hero  */}
+        {/* Benefits  */}
+        {/* Collaboration  */}
+        {/* Services  */}
+        {/* Pricing  */}
+        {/* Roadmap  */}
+        {/* Footer */}
+      </div>
+      {/* ButtonGradients */}
+    </>
+  );
+}
+
+export default App
